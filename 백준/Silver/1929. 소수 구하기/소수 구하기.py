92c75231f9d5020a92b71ffc1b289af11636f153
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-09 13:22:59 +0900

    [Silver III] Title: 소수 구하기, Time: 684 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,23 +1,19 @@
-# 10시 39분~
 from sys import stdin
-import math
 
 m, n = map(int, stdin.readline().split())
 
-for mn in range(m, n+1):
-    r = True
-    if mn == 1:
-        continue
-    if mn <= 5:
-        for i in range(2, mn):
-            if mn%i==0:
-                r = False
-                break
-    else:
-        for i in range(2, int(math.sqrt(mn))+1):
-            if mn%i==0:
-                r = False
-                break
+arr = [True] * (n + 1)
+arr[0] = False
+arr[1] = False
 
-    if r==True:
-        print(mn)
+for i in range(2,n+1):
+    if arr[i] == True:
+        j = 2
+        while i*j <= n:
+            arr[i*j] = False
+            j+=1
+
+
+for i in range(m, n+1):
+    if arr[i] == True:
+        print(i)

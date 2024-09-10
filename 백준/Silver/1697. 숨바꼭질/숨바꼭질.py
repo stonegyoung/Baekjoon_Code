756c1da38ab07786e0f0c889bddbd8a688e04333
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-10 20:47:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 84 ms, Memory: 34268 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -6,14 +6,17 @@ n, k = map(int, stdin.readline().split())
 
 visited = [0] * 100001
 
-def find_syster(n, k):
+def find_syster(n, k):    
+    if n > k:
+        return n - k
+    
     queue = deque([n])
     
     while queue:
         v = queue.popleft()
         if v == k:
             return visited[k]
-        
+         
         for x in [v+1, 2*v, v-1]:
             if 0 <= x < len(visited):
                 if visited[x]==0:    

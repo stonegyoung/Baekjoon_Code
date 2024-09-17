826847a2ed321a16f2a35c360090e30ea670b6a4
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-17 17:05:01 +0900

    [Gold V] Title: 무한 수열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -7,14 +7,7 @@ def dp(n, p, q):
     if n in arr:
         return arr[n]
     
-    if n//p in arr and n//q in arr:
-        arr[n] = arr[n//p] + arr[n//q]
-        return arr[n]
-    elif n//q in arr:
-        return arr[n//q] + dp(n//p, p, q)
-    elif n//p in arr:
-        return arr[n//p] + dp(n//q, p, q)
-    else:
-        return dp(n//p, p, q) + dp(n//q, p, q)
+    arr[n] = dp(n//p, p, q) + dp(n//q, p, q)
+    return arr[n]
     
 print(dp(n,p,q))

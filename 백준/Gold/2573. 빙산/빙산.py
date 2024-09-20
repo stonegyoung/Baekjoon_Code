f0341314a5f7c4e81fef0ad97a1fa62320f0fead
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-20 15:05:30 +0900

    [Gold IV] Title: 빙산, Time: 1168 ms, Memory: 211472 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,5 +1,5 @@
 # 10시 18분~10tl30
-# 10시 45분 11시 14qns
+# 10시 45분 11시 35qns
 
 # 상하좌우 -> 몇인지 넣을 리스트
 # 두 덩어리 이상이 되는 지 확인 분리 되는 지 확인 안되면 0 -> bfs
@@ -40,35 +40,33 @@ for _ in range(n):
     graph.append(list(map(int, input().split())))
     
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
+       
+nonzero = {}
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] != 0:
+            nonzero[(i,j)] = count_0(i,j)
+            
 result = 0
 cnt = 0
 while graph != [[0]*m for _ in range(n)]:
-    zero = [[] for _ in range(n)]
-
-    for i in range(n):
-        for j in range(m):
-            zero[i].append(count_0(i, j))
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] != 0:
-                graph[i][j] = graph[i][j]-zero[i][j] if graph[i][j]-zero[i][j]>0 else 0
+    # 좌표와 빙산이 - 되는 걸 같이 적는 리스트 
+    # 좌표 중에 -했더니 녹은 거면 그 근처 애들 카운트 다시
+    for x, y in nonzero:
+        graph[x][y] = max(0, graph[x][y] - nonzero[(x,y)])
+    for x, y in nonzero:
+        if graph[x][y] != 0:
+            nonzero[(x,y)] = count_0(x,y) # 다시 업데이트
     cnt += 1
-    # print(graph)
     visited = [[True]*m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                visited[i][j] = False
-    # print(visited)
+    
     res = 0
     for i in range(n):
         for j in range(m):
-            if visited[i][j]: # True면
+            if visited[i][j]==True and graph[i][j]!=0:
                 dfs(graph, visited, i,j) 
                 res += 1
                 # print(i, j)
-    
     if res > 1:
         result = cnt
         break

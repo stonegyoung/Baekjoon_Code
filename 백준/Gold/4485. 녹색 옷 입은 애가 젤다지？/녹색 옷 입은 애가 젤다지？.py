01cf1907df6d147ea3e3af5bc128815392800324
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-27 23:09:05 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 100 ms, Memory: 34656 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,63 @@
+# 9시 58분
+import sys
+from collections import deque
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+directions = [(-1,0), (1,0), (0,-1), (0,1)]
+def bfs(graph, visited, x,y):
+    queue = deque([(x,y)])
+    visited[x][y] = graph[x][y]
+    
+    while queue:
+        nowx, nowy = queue.popleft()
+        for dx, dy in directions:
+            x = nowx + dx
+            y = nowy + dy
+            
+            if 0<=x<n and 0<=y<n:
+                if visited[x][y]==0:
+                    visited[x][y] = visited[nowx][nowy] + graph[x][y]
+                    queue.append((x,y))
+                else:
+                    if visited[x][y] > visited[nowx][nowy] + graph[x][y]:
+                        visited[x][y] = visited[nowx][nowy] + graph[x][y]
+                        queue.append((x,y))
+    
+def dijkstra(graph, visited, x,y):
+    q = []
+    heapq.heappush(q, (graph[x][y], x, y))
+    visited[x][y] = graph[x][y]
+    while q:
+        cost, nowx, nowy = heapq.heappop(q)
+        for dx, dy in directions:
+            x = nowx + dx
+            y = nowy + dy
+            
+            if 0<=x<n and 0<=y<n:
+                if visited[x][y] < cost + graph[x][y]:
+                    continue
+                
+                costs = cost + graph[x][y]
+                if visited[x][y] > costs:
+                    visited[x][y] = costs
+                    heapq.heappush(q, (visited[x][y], x, y))
+                
+# bfs
+cnt = 1
+while True:
+    n = int(input())
+    
+    if n == 0:
+        break
+    
+    graph = []
+    visited = [[INF]*n for _ in range(n)]
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
+        
+    dijkstra(graph, visited, 0, 0)
+    # print(visited)
+    print(f'Problem {cnt}: {visited[n-1][n-1]}')
+    cnt+=1
+        

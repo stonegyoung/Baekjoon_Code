b2db49dcedf1765219ada2ed3fb132392de2cf1e
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-28 17:00:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 980 ms, Memory: 105932 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,25 +1,20 @@
-# 4시 20분
+# 4시 20분~37분
 # 누적합
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-sum_graph = [[0]*(n+1)]
-for _ in range(n):
-    lists = (list(map(int, input().split())))
-    sumlists = [0] * (n+1)
-    for i in range(1, n+1):
-        sumlists[i] = sumlists[i-1] + lists[i-1]
-    sum_graph.append(sumlists)
-    
-# print(sum_graph)
 
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+sum_graph = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sum_graph[i][j] = sum_graph[i-1][j] + sum_graph[i][j-1] - sum_graph[i-1][j-1] + graph[i-1][j-1] 
+        
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
+    print(sum_graph[x2][y2] -sum_graph[x2][y1-1] -sum_graph[x1-1][y2] + sum_graph[x1-1][y1-1])
     
-    summ = 0
-    # x1행부터x2행까지
-    for x in range(x1, x2+1):
-        # y1열~y2열까지
-        summ += sum_graph[x][y2] - sum_graph[x][y1-1]
-    print(summ)
+        

a3e47f5519423ceee0b264a49a5ee31660069f58
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-09-29 01:05:07 +0900

    [Silver III] Title: N과 M (5), Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,27 +1,40 @@
-# 12시 19분
+# 12시 19분~51분
 import sys
 input = sys.stdin.readline
 
-def bt(idx, letter):
+# def bt(idx, letter):
+#     if idx >= m:
+#         print(' '.join(letter))
+#         return
+    
+#     for i in lists:
+#         if check(letter, str(i)):
+#             bt(idx+1, letter+[str(i)])
+            
+# def check(letter, s):
+#     if len(letter) == 0:
+#         return True
+#     if s in letter:
+#         return False
+#     else:
+#         return True
+    
+def dfs(idx, letter):
     if idx >= m:
         print(' '.join(letter))
         return
     
-    for i in lists:
-        if check(letter, str(i)):
-            bt(idx+1, letter+[str(i)])
-            
-def check(letter, s):
-    if len(letter) == 0:
-        return True
-    if s in letter:
-        return False
-    else:
-        return True
-    
+    for i in range(len(lists)):
+        if visited[i]:
+            continue
+        visited[i] = True
+        dfs(idx+1, letter+[str(lists[i])])
+        visited[i] = False
+        
 n, m = map(int, input().split())
 
 lists=list(map(int, input().split()))
 lists.sort()
-
-bt(0, [])
+visited = [False] * n
+dfs(0, [])
+# bt(0, [])

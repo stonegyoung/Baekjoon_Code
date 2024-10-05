4f1106fbb5d7e42e9b382e39c9192d5ea824918b
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-10-05 14:51:33 +0900

    [Gold IV] Title: 운동, Time: 768 ms, Memory: 111924 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -7,9 +7,6 @@ INF = int(1e9)
 v, e = map(int, input().split())
 
 graph = [[INF]*(v+1) for _ in range(v+1)]
-
-for i in range(1, v+1):
-    graph[i][i] = 1
     
 for _ in range(e):
     a, b, c = map(int, input().split())
@@ -22,13 +19,7 @@ for k in range(1, v+1):
  
 mini = INF
 for i in range(1, v+1):
-    for j in range(1, v+1): 
-        if i==j:
-            continue
-        a = graph[i][j]
-        b = graph[j][i]
-        if a == INF or b == INF:
-            continue
-        mini = min(mini, a+b)
+    if graph[i][i] != INF:
+        mini = min(mini, graph[i][i])
         
 print(mini if mini != INF else -1)

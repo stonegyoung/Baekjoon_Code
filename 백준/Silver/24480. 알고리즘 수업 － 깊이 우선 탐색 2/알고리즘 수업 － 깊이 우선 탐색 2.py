bbddfe194a382dbe9822f8e103b848b01bcdca73
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-10-05 15:34:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 612 ms, Memory: 59540 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,31 @@
+# 3시 12분
+# dfs
+import sys
+input = sys.stdin.readline
+
+# cnt = 1
+def dfs(start, graph, visited):
+    cnt = 1
+    stack = [start]
+    while stack:
+        node = stack.pop()
+        if visited[node]==0:
+            visited[node] = cnt
+            cnt += 1
+        for n in graph[node]:
+            if visited[n] == 0:
+                stack.append(n)
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+for g in graph:
+    g.sort()
+dfs(r, graph, visited)
+for v in visited[1:]:
+    print(v)

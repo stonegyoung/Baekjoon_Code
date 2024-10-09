a752000d3c17335491a2b62e30ce12b6d3ad3823
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-10-09 20:28:32 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 2336 ms, Memory: 115456 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,54 +1,38 @@
-# 12시 44분
-# 플워 -> 다익
 import sys
-import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-def dijkstra(start, end, graph, distance):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    
-    while q:
-        cost, node = heapq.heappop(q)
-        
-        if cost > distance[node]:
-            continue
-        for i in graph[node]: # i[0]은 cost, i[1]은 노드
-            c = cost + i[0]
-            if c < distance[i[1]]:
-                distance[i[1]] = c
-                heapq.heappush(q, (c, i[1]))
-    return distance[end] 
+def fw(graph):
+    for k in range(1, n+1):
+        gk = graph[k]
+        for i in range(1, n+1):
+            gi = graph[i]
+            for j in range(1, n+1):
+                if i == j:
+                    continue
+                gi[j] = min(gi[k]+gk[j], gi[j])
 
 n, e = map(int, input().split())
-g =  [[] for _ in range(n+1)]
+
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    graph[i][i] = 0
+    
 for _ in range(e):
     a, b, c = map(int, input().split())
-    g[a].append((c, b))
-    g[b].append((c, a))
-    
+    graph[a][b] = c
+    graph[b][a] = c
+
+fw(graph)
+
 v1, v2 = map(int, input().split())
-
-def min_distance(s, e):
-    if s == e:
-        return 0
-    distance = [INF] * (n+1)
-    return dijkstra(s, e, g, distance)
-
-tov1 = min_distance(1, v1)
-v1tov2 = min_distance(v1, v2)
-v2ton = min_distance(v2, n)
-tov2 = min_distance(1, v2)
-v2tov1 = min_distance(v2, v1)
-v1ton = min_distance(v1, n)
-
-res = min(
-        (tov1 + v1tov2 + v2ton), # 1 -> v1 -> v2 -> n
-        (tov2 + v2tov1 + v1ton), # 1 -> v2 -> v1 -> n
-    )
-if res >= INF:
+res = min(graph[1][v1] + graph[v1][v2] + graph[v2][n], 
+          graph[1][v2] + graph[v2][v1] + graph[v1][n],
+          graph[1][v1] + graph[v1][v2] + graph[v2][v1] + graph[v1][n],
+          graph[1][v2] + graph[v2][v1] + graph[v1][v2] + graph[v2][n]          
+        )
+if res >= INF :
     print(-1)
 else:
     print(res)

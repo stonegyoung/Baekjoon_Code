6a5360ad5a184944077ceac00ccea5e4c87f769b
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-10-14 20:32:36 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 1512 ms, Memory: 33628 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,52 +1,20 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
- 
-# def post(node):
-#     if node.left != None:
-#         post(node.left)    
-#     if node.right != None:
-#         post(node.right)
-#     print(node.node)
 
-# class Node:
-#     def __init__(self, node):
-#         self.node = node
-#         self.left = None
-#         self.right = None
-    
-#     def plus(self, res):
-#         if self.left == None and self.node>res.node:
-#             self.left = res
-#         elif self.right == None and self.node<res.node:
-#             self.right = res
-            
-#         elif self.node > res.node:
-#             self.left.plus(res)
-#         else:
-#             self.right.plus(res)
-# i = True
-# while True:
-#     try:
-#         if i:
-#             root  = Node(int(input()))
-#             i = False
-#         else:
-#             a = Node(int(input()))
-#             root.plus(a)
-#     except:
-#         break
-# post(root)
-   
-def post(lists):
-    mid = lists[0]
-    left = [i for i in lists if mid>i]
-    right = [i for i in lists if mid<i]
-    if len(left) != 0:
-        post(left)
-    if len(right) != 0:
-        post(right)
+def post(start, end):
+    if start > end:
+        return
+    mid = lists[start]
+    change = end+1
+    for i in range(start+1, end+1):
+        if mid < lists[i]:
+            change = i
+            break
+    post(start+1, change-1)
+    post(change, end)
     print(mid)
+        
     
 lists = []
 while True:
@@ -54,4 +22,4 @@ while True:
         lists.append(int(input()))
     except:
         break
-post(lists)
+post(0, len(lists)-1)

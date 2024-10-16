7c399b73464f7c5a916902c0255ecb7eb69febf3
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-10-16 15:30:55 +0900

    [Silver I] Title: 카드 구매하기, Time: 188 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,17 @@
+# 3시 18분
+# dp
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nlist = list(map(int, input().split()))
+
+dp = [0] + nlist
+# print(dp)
+for i in range(2, n+1):
+    for j in range(1, i):
+        dp[i] = max(dp[i], dp[j]+dp[i-j])
+        # print(dp)
+        
+print(dp[n])

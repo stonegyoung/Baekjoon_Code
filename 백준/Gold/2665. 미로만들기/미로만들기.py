450fcd5f05b9189d473373189b07952c55bf2b93
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2024-11-09 15:16:34 +0900

    [Gold IV] Title: 미로만들기, Time: 64 ms, Memory: 36316 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,4 +1,4 @@
-# 11 21
+# 11 21~12시 25분
 # bfs
 # 1이면 다 0으로 초기화 1이면 +1
 
@@ -8,49 +8,34 @@ from collections import deque
 INF = int(1e9)
 input = sys.stdin.readline
 
-def bfs(startx, starty):
-    q = deque([(startx, starty)])
+def dijkstra(startx, starty):
+    q = []
+    heapq.heappush(q, (0,startx,starty))
     visited[startx][starty] = 0
     while q:
-        nowx, nowy = q.popleft()
+        last, nowx, nowy = heapq.heappop(q)
         for dx, dy in [(-1,0), (1,0), (0,1), (0,-1)]:
             x = nowx+dx
             y = nowy+dy
             if 0<=x<n and 0<=y<n:
-                last = visited[nowx][nowy]
-                if graph[x][y] == 1:
-                    if visited[x][y] == -1: # -1이고 visit x
+                if last > visited[x][y]:
+                    continue
+                if graph[x][y] == 1:                        
+                    if last < visited[x][y]:
                         visited[x][y] = last
-                        q.append((x,y))
-                    else :
-                        if visited[x][y] > last:
-                            visited[x][y] = last
-                            q.append((x,y))
+                        heapq.heappush(q, (last,x,y))
                 else:
-                    if visited[x][y] == INF:
-                        visited[x][y] = last + 1
-                        q.append((x,y))
-                    else :
-                        if visited[x][y] > last+1:
-                            visited[x][y] = last+1
-                            q.append((x,y))
+                    if last+1 < visited[x][y]:
+                        visited[x][y] = last+1
+                        heapq.heappush(q, (last+1,x,y))
     return visited[n-1][n-1]
-                
-        
 n=int(input())
 graph = []
-visited = []
+visited = [[INF]*n for _ in range(n)]
 for _ in range(n):
     lists = list(map(int, input().rstrip()))
     graph.append(lists)
-    g = []
-    for i in lists:
-        if i == 1:
-            g.append(-1)
-        else:
-            g.append(INF)
-    visited.append(g)
 
-a = bfs(0,0)
-print(a)
+a = dijkstra(0,0)
 # print(visited)
+print(a)

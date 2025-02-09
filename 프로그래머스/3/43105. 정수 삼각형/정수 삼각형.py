01976d31698a1a9c64a36fc58e0b1abcdd92ea29
stonegyoung <127651962+stonegyoung@users.noreply.github.com>
2025-02-09 15:28:48 +0900

    [level 3] Title: 정수 삼각형, Time: 53.72 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,10 +1,8 @@
 # 3: 11
 # dp
-# 0아니면 +1로
+# 0아니면 -1로
 def solution(triangle):
     answer = 0
-    # dp = [[] for _ in range(len(triangle))]
-    # dp[0] = triangle[0]
     for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
             if j == 0:
